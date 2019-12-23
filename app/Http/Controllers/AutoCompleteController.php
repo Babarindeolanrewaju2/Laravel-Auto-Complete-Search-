c1b1d4932ca8610caab01d10af30c7d2e125c01f
ayuba <olanrewajuayuba@gmail.com>
2019-12-23 20:44:07 +0100

    added the blade file and completed the project

--- app/Http/Controllers/AutoCompleteController.php
+++ app/Http/Controllers/AutoCompleteController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class AutoCompleteController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('search');
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->get('term');
+        $result = User::where('name', 'LIKE', '%' . $search . '%')->get();
+        return response()->json($result);
+
+    }
 }
